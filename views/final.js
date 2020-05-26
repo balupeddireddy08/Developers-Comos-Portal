1d344b462c64d54343877cefef49a2884e679ec7
sbk9908 <bajisbk786@gmail.com>
2020-05-26 12:03:51 +0530

    checking out login

--- views/final.js
+++ views/final.js
@@ -11,9 +11,36 @@ const saltRounds = 10;
 const LocalStrategy = require('passport-local').Strategy;
 
 const session = require("express-session");
-const passportLocalMongoose = require("passport-local-mongoose");
+// const passportLocalMongoose = require("passport-local-mongoose");
 const findOrCreate = require("mongoose-findorcreate");
 const app = express();
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.use(session({
+  secret:"Thisisnotmysecret",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+mongoose.connect("mongodb://localhost:27017/LoginDB",{useNewUrlParser:true,useUnifiedTopology:true});
+mongoose.set("useCreateIndex",true);
+
+const userSchema = new mongoose.Schema({
+  name :String,
+  email : String,
+  password : String,
+  googleId: String
+});
+
+// userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+const User = mongoose.model("user",userSchema);
 passport.use(new LocalStrategy(
   function(email, password, done) {
     User.findOne({ email: email }, function(err, user) {
@@ -34,6 +61,7 @@ passport.use(new LocalStrategy(
     });
   }
 ));
+
 passport.serializeUser(function(user, done) {
   done(null, user.id);
 });
@@ -43,30 +71,18 @@ passport.deserializeUser(function(id, done) {
     done(err, user);
   });
 });
-app.use(express.static("public"));
-app.set('view engine','ejs');
-app.use(bodyParser.urlencoded({extended:true}));
-
-app.use(session({
-  secret:"Thisisnotmysecret",
-  resave: false,
-  saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
-
-mongoose.connect("mongodb://localhost:27017/LoginDB",{useNewUrlParser:true,useUnifiedTopology:true});
-mongoose.set("useCreateIndex",true);
-
-const userSchema = new mongoose.Schema({
-  name :{type: String,unique:true},
-  email : {type:String,unique:true},
-  password : String
-});
-userSchema.plugin(passportLocalMongoose);
-userSchema.plugin(findOrCreate);
-const User = mongoose.model("user",userSchema);
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/welcome",
+    userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 
 app.use(function(req, res, next) {
@@ -78,6 +94,14 @@ app.use(function(req, res, next) {
 app.get("/",function(req,res){
   res.render("home");
 });
+app.get("/auth/google",passport.authenticate("google",{scope:["profile"]})
+);
+app.get("/auth/google/welcome",
+  passport.authenticate("google",{failureRedirect:"/login"}),
+  function(req,res){
+    res.redirect("/welcome");
+});
+
 app.get("/login",function(req,res){
   res.render("login");
 });
@@ -95,7 +119,7 @@ app.post("/register",function(req,res){
     errors.push({ msg: 'Please enter all fields' });
   }
 
-  if (password != password2) {
+  if (password !== password2) {
     errors.push({ msg: 'Passwords do not match' });
   }
 
@@ -150,8 +174,7 @@ app.post("/register",function(req,res){
 });
 app.post('/login',
   passport.authenticate('local', { successRedirect: '/welcome',
-                                   failureRedirect: '/login',
-                                   failureFlash: true })
+                                   failureRedirect: '/login' })
 );
 
 app.get('/logout', function(req, res){
